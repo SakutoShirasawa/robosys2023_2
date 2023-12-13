edcb86fef6dc5db269a48577905e0e8c28ad49cb
Sakuto Shirasawa <ss3910business@gmail.com>
2023-12-14 08:35:32 +0900

    change talker and listener

--- ros2_ws/src/mypkg/build/mypkg/prefix_override/sitecustomize.py
+++ ros2_ws/src/mypkg/build/mypkg/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/sakutoshirasawa/ros2_ws/src/mypkg/install/mypkg'
+sys.prefix = sys.exec_prefix = '/home/sakutoshirasawa/robosys2023_2/ros2_ws/src/mypkg/install/mypkg'
